f19dae57b07de52f9e6d595b4f538b35bd1bd2f4
dsantucho <soledadsantucho@hotmail.com>
2023-05-22 21:41:09 -0300

    banner products mobile done

---
+++ src/components/BannerProducts.js
@@ -0,0 +1,38 @@
+import React from 'react';
+//img
+
+
+const BannerProducts = () => {
+    const styleImgProduct1 = {
+        backgroundImage: 'url("https://firebasestorage.googleapis.com/v0/b/estetica-adriana-aguilar.appspot.com/o/products%2Fproduct-1.png?alt=media")'
+    };
+    const styleImgProduct2 = {
+        backgroundImage: 'url("https://firebasestorage.googleapis.com/v0/b/estetica-adriana-aguilar.appspot.com/o/products%2Fproduct-4.png?alt=media")'
+    };
+    const styleImgProduct3 = {
+        backgroundImage: 'url("https://firebasestorage.googleapis.com/v0/b/estetica-adriana-aguilar.appspot.com/o/products%2Fproduct-3.png?alt=media")'
+    };
+  return (
+    <div className='flex-auto min-w-full my-5 bg-sc-background'>
+        <h2 className='banner-services-h2 p-6 text-center text-3xl leading-8 tracking-wider text-typo-color whitespace-pre-line'>
+        Nuestros productos <br /> &amp; <br /> Tecnologías</h2>
+        <div className='flex flex-col items-center'>
+            <div className='h-[140px] w-[140px] overflow-hidden flex flex-col items-center bg-cover bg-center my-6'
+            style={styleImgProduct1}></div>
+            <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center my-2 px-3'>Productos de alta calidad aprobados por expertos en el cuidado de la piel</span>
+        </div>
+        <div className='flex flex-col items-center'>
+            <div className='h-[140px] w-[140px]  flex flex-col items-center bg-contain bg-no-repeat bg-center my-6'
+            style={styleImgProduct2}></div>
+            <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center my-2 px-3'>Técnicas avanzadas en tecnología de belleza.</span>
+        </div>
+        <div className='flex flex-col items-center'>
+            <div className='h-[140px] w-[140px] overflow-hidden flex flex-col items-center bg-cover bg-center my-6'
+            style={styleImgProduct3}></div>
+            <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center mt-2 mb-6 px-3'>Productos exclusivos y seleccionados a nivel internacional para brindar los mejores resultados</span>
+        </div>
+    </div>
+  )
+}
+
+export default BannerProducts
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import React from 'react'
 //components
 import BannerHeader from '../components/BannerHeader';
 import BannerServices from '../components/BannerServices';
+import BannerProducts from '../components/BannerProducts';
 
 const Home = () => {
   return (
     <div className='flex flex-col w-screen bg-background'>
         <BannerHeader/>  
         <BannerServices/>  
+        <BannerProducts/>
     </div>
   )
 }

8740b94d40bd3b02430a148d8c029378901bc8c0
dsantucho <soledadsantucho@hotmail.com>
2023-05-25 17:58:06 -0300

    footer back arreglado

--- src/components/BannerFooter.js
+++ src/components/BannerFooter.js
@@ -5,21 +5,21 @@ const BannerFooter = () => {
         backgroundImage: 'url("https://firebasestorage.googleapis.com/v0/b/estetica-adriana-aguilar.appspot.com/o/footer%2Ffooter-vec-logo-adri.png?alt=media")'
     };
   return (
-    <div className='bg-footer-background flex flex-col items-center' >
-        <div className='flex flex-col w-[340px] text-footer-typo-color my-4 bg-cover bg-center overflow-hidden' style={footerImg}>
-            <h2 className='text-2xl font-bold leading-8 tracking-wider'>Servicio</h2>
+    <div className='bg-footer-background flex flex-col items-center pt-3' >
+        <div className='flex flex-col w-[340px] text-footer-typo-color my-4 bg-cover bg-center overflow-hidden opacity-75 footer-background'>
+            <h2 className='text-2xl font-bold leading-8 tracking-wider mt-3 mb-1'>Servicio</h2>
             <span className='font-light text-sm tracking-wider my-1'>Relax</span>
             <span className='font-light text-sm tracking-wider my-1'>Depilacion</span>
             <span className='font-light text-sm tracking-wider my-1'>Tratamientos Corporales</span>
             <span className='font-light text-sm tracking-wider my-1'>Tratamientos Masculinos</span>
-            <h2 className='text-2xl font-bold leading-8 tracking-wider'>Horarios de Atencion</h2>
+            <h2 className='text-2xl font-bold leading-8 tracking-wider mt-3 mb-1'>Horarios de Atencion</h2>
             <span className='font-light text-sm tracking-wider my-1'>Lunes a Viernes 10am - 19pm</span>
-            <h2 className='text-2xl font-bold leading-8 tracking-wider'>Explorar</h2>
+            <h2 className='text-2xl font-bold leading-8 tracking-wider mt-3 mb-1'>Explorar</h2>
             <span className='font-light text-sm tracking-wider my-1'>Home</span>
             <span className='font-light text-sm tracking-wider my-1'>Servicios</span>
             <span className='font-light text-sm tracking-wider my-1'>Sobre nosotros</span>
             <span className='font-light text-sm tracking-wider my-1'>Contacto</span>
-            <h2 className='text-2xl font-bold leading-8 tracking-wider'>Contactos</h2>
+            <h2 className='text-2xl font-bold leading-8 tracking-wider mt-3 mb-1'>Contactos</h2>
             <span className='font-light text-sm tracking-wider my-1'>Direccion: 1234 Carrasco, Montevideo</span>
             <span className='font-light text-sm tracking-wider my-1'>Whatsapp: +589 94943434</span>
             <span className='font-light text-sm tracking-wider my-1'>Redes</span>

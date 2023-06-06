8b3017e9bf43c460ce8fa1e9ea63efa37da68f02
dsantucho <soledadsantucho@hotmail.com>
2023-06-06 17:28:12 -0300

    banner product responsive done

--- src/components/BannerProducts.js
+++ src/components/BannerProducts.js
@@ -14,22 +14,29 @@ const BannerProducts = () => {
     };
   return (
     <div className='flex-auto min-w-full my-5 bg-sc-background'>
-        <h2 className='banner-services-h2 p-6 text-center text-3xl leading-8 tracking-wider text-typo-color whitespace-pre-line'>
-        Nuestros productos <br /> &amp; <br /> Tecnologías</h2>
-        <div className='flex flex-col items-center'>
-            <div className='h-[140px] w-[140px] overflow-hidden flex flex-col items-center bg-cover bg-center my-6'
-            style={styleImgProduct1}></div>
-            <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center my-2 px-3'>Productos de alta calidad aprobados por expertos en el cuidado de la piel</span>
+        <div>
+            <h2 className=' banner-services-h2 p-6 text-center text-3xl leading-8 tracking-wider text-typo-color lg:hidden'>
+            Nuestros productos <br /> &amp; <br /> Tecnologías</h2>
+            <h2 className='banner-services-h2 p-6 text-center text-5xl leading-8 tracking-wider text-typo-color hidden lg:block'>
+          Nuestros productos &amp; Tecnologías
+        </h2>
         </div>
-        <div className='flex flex-col items-center'>
-            <div className='h-[140px] w-[140px]  flex flex-col items-center bg-contain bg-no-repeat bg-center my-6'
-            style={styleImgProduct2}></div>
-            <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center my-2 px-3'>Técnicas avanzadas en tecnología de belleza.</span>
-        </div>
-        <div className='flex flex-col items-center'>
-            <div className='h-[140px] w-[140px] overflow-hidden flex flex-col items-center bg-cover bg-center my-6'
-            style={styleImgProduct3}></div>
-            <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center mt-2 mb-6 px-3'>Productos exclusivos y seleccionados a nivel internacional para brindar los mejores resultados</span>
+        <div className='lg:flex lg:justify-around'>
+            <div className='flex flex-col items-center md:px-16 lg:px-2 lg:w-[400px] lg:m-2'>
+                <div className='h-[140px] w-[140px] overflow-hidden flex flex-col items-center bg-cover bg-center my-6'
+                style={styleImgProduct1}></div>
+                <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center my-2 px-3'>Productos de alta calidad aprobados por expertos en el cuidado de la piel</span>
+            </div>
+            <div className='flex flex-col items-center md:px-16 lg:px-2 lg:w-[400px] lg:m-2'>
+                <div className='h-[140px] w-[140px]  flex flex-col items-center bg-contain bg-no-repeat bg-center my-6'
+                style={styleImgProduct2}></div>
+                <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center my-2 px-3'>Técnicas avanzadas en tecnología de belleza.</span>
+            </div>
+            <div className='flex flex-col items-center md:px-16 lg:px-2 lg:w-[400px] lg:m-2'>
+                <div className='h-[140px] w-[140px] overflow-hidden flex flex-col items-center bg-cover bg-center my-6'
+                style={styleImgProduct3}></div>
+                <span className='text-xl font-extralight tracking-wider leading-6 text-black text-center mt-2 mb-6 px-3'>Productos exclusivos y seleccionados a nivel internacional para brindar los mejores resultados</span>
+            </div>
         </div>
     </div>
   )

f983fc5694a21e87f7ace18a66380dc01a32223d
dsantucho <soledadsantucho@hotmail.com>
2023-06-23 17:21:25 -0300

    fix img cardTreatment y nav space

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
     let [open, setOpen] = useState(false);
     return (
         <div className='shadow-md w-full fixed top-0 left-0  bg-background z-50'>
-            <div className='flex justify-between pb-4 md:inline '>
+            <div className='flex justify-between pb-4 md:inline fix-max-width'>
                 <div className='flex justify-center'>
                     <img alt='mobileLogo' src={navMobileLogo} className='object-contain p-2 my-2 flex w-10/12 md:w-96'></img>
                 </div>
@@ -27,7 +27,7 @@ export default function Navbar() {
                     <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
                 </div>
 
-                <ul className={`md:h-12 md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:justify-around pl-9 md:pl-0 transition-none  bg-background ${open ? 'top-32' : 'top-[-490px] borderNav'} ${open ? '': 'borderNav'}`}>
+                <ul className={`md:h-12 md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:justify-evenly pl-9 md:pl-0 transition-none  bg-background ${open ? 'top-32' : 'top-[-490px] borderNav'} ${open ? '': 'borderNav'}`}>
                     {
                         navigation.map((navigation) => (
                             <li key={navigation.name} className='md:ml-8 text-xl md:my-0 my-7'>

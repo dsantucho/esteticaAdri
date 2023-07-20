527189150244225b09889e318d4691ec13e04858
dsantucho <soledadsantucho@hotmail.com>
2023-07-20 19:24:45 -0300

    fix size bannerService

--- src/components/BannerServices.js
+++ src/components/BannerServices.js
@@ -13,9 +13,15 @@ const BannerServices = () => {
 
 
   return (
-    <div className='flex-auto min-w-full my-5'>
-        <h2 className='banner-services-h2 p-5 text-center text-3xl leading-8 tracking-wider text-typo-color lg:text-5xl lg:text-left'>Descubrí nuestros servicios</h2>
-        <div className='flex flex-col items-center lg:flex-row lg:justify-center'>
+    <div className='flex flex-col min-w-full my-5 lg:max-w-5xl'>
+            <div className='lg:hidden flex flex-row justify-center'>
+                <h2 className='banner-services-h2 py-5 text-center text-2xl leading-8 tracking-wider text-typo-color '>Descubrí nuestros servicios</h2>
+            </div>
+            <div className='hidden lg:flex lg:flex-row lg:justify-start lg:max-w-5xl lg:ml-10 xl:ml-20 2xl:ml-40'>
+                <h2 className='banner-services-h2 py-5 leading-8 tracking-wider text-typo-color text-3xl text-left'>Descubrí nuestros servicios</h2>
+            </div>
+            
+        <div className='flex flex-col items-center lg:max-w-5xl lg:flex-row lg:justify-self-center lg:self-center'>
             {
                 services.map((services) => (
                     <ServicesCard data = {services} key={services.name}/>

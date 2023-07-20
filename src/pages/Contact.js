7d355dbca1bb017070bc7b480f64f7fc8bc698ce
dsantucho <soledadsantucho@hotmail.com>
2023-07-20 13:13:28 -0300

    contact basic

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import BannerContact from "../components/BannerContact";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const phoneNumber = "+59899262217";
+  const whatsappUrl = `https://wa.me/${phoneNumber}`;
 
-export default Contact
+  return (
+    <div className=" bg-sc-background flex flex-row justify-center items-center mt-4">
+      <div className="lg:w-full">
+        <BannerContact />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
